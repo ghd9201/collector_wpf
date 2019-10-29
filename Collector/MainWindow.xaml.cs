a5d376dc3dba8a6961465edd1e9216a1ab13af2d
ghd9201 <ghd9201@naver.com>
2019-10-29 18:49:03 +0900

    need to make button event

---
+++ Collector/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Collector
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPopupLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HistoryMenuBotton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImprovementMenuBotton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FixMenuBotton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RatingMenuBotton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HomeMenuBotton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
